5069d09498214b30fb7fd298c9475d007d44493d
c thamarai kani <33809429+thamaraikani69@users.noreply.github.com>
2018-11-12 22:53:48 +0530

    Update whats-app-people-online-indicator.py

--- whats-app-people-online-indicator.py
+++ whats-app-people-online-indicator.py
@@ -3,55 +3,44 @@ from selenium import webdriver
 import time
 
 import json
-from urllib.request import urlopen,Request
+from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 import time
 
 from datetime import datetime
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
 
 def target():
-
-    driver=webdriver.Chrome("C:\\chromedriver.exe")
+    driver = webdriver.Chrome("C:\\chromedriver.exe")
     driver.get("https://web.whatsapp.com/")
-
 
     input("scan the qr code using mobile phone manually")
 
     time.sleep(3)
 
+    search=driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label')
     search_name = input('enter the name here')
     search.send_keys(search_name)
-    input('enter1')
+    input('After typing a name press enter ')
 
-
-    search_click=driver.find_element_by_xpath('//*[@id="pane-side"]/div/div/div/div[2]/div').click()
+    search_click = driver.find_element_by_xpath('//*[@id="pane-side"]/div/div/div/div[2]/div').click()
     time.sleep(3)
     print(datetime.now())
     online(driver)
 
-def online(driver):
 
-    current_status=driver.find_element_by_class_name('O90ur').text
-    print('current status:',current_status)
+def online(driver):
+    current_status = driver.find_element_by_class_name('O90ur').text
+    print('current status:', current_status)
 
     while '1' == '1':
-        check_fun=driver.find_element_by_class_name('O90ur').text
-
+        check_fun = driver.find_element_by_class_name('O90ur').text
 
         if check_fun == 'online':
 
-
             print('online\n')
             print(check_fun + '\n')
-            print(datetime.now()+'\n')
+            print(str(datetime.now()) + '\n')
             message_send_call(driver)
 
 
@@ -59,91 +48,81 @@ def online(driver):
             None
 
 
-
-
-
 def message_send_call(driver):
     TO = message_sent
-    times=datetime.now()
+    times = datetime.now()
     message = '''online\n''' + str(times)
 
     mob = mobile_num
     passw = passw_pass
     try:
 
+        #apikey = apikey
+        # examole apikey="osamav92OdWuZRaSfDrXmM95N3e472"
+        api = apikey
 
-                        apikey=apikey
-                        #examole apikey="osamav92OdWuZRaSfDrXmM95N3e472"
-                        api = apikey
+        headers = {"Accept-Language": "en-US,en;q=0.5",
+                   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
+                   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                   "Referer": "http://thewebsite.com",
+                   "Connection": "keep-alive"}
 
-                        headers = {"Accept-Language": "en-US,en;q=0.5",
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-"Referer": "http://thewebsite.com",
-"Connection": "keep-alive"}
+        values = {
+            'Mobile': mob,
+            'Password': passw,
+            'Message': message,
+            'To': TO,
+            'Key': api
+        }
+        datas = urlencode(values)
+        data = datas.encode('ascii')
+        urls = "https://smsapi.engineeringtgr.com/send/"
+        url = urls + '?' + datas
+        req = Request(url, data, headers)
+        response = json.loads(urlopen(req).read())
+        print(response)
 
-                        values={
-                            'Mobile':mob,
-                            'Password':passw,
-                            'Message': message,
-                            'To': TO,
-                            'Key':api
-                        }
-                        datas = urlencode(values)
-                        data=datas.encode('ascii')
-                        urls= "https://smsapi.engineeringtgr.com/send/"
-                        url=urls+'?'+datas
-                        req = Request(url,data,headers)
-                        response = json.loads(urlopen(req).read())
-                        print(response)
+        if response['status'] == "False":
 
+            print("check apikey")
+            message_send_call()
 
-                        if response['status'] == "False":
+        else:
 
-                         print("check apikey")
-                         message_send_call()
-
-                        else:
-
-                         print("SMS Send Successfully")
-                         print(datetime.now())
-                         time.sleep(10)
-                         logout_waiting(driver)
+            print("SMS Send Successfully")
+            print(datetime.now())
+            time.sleep(10)
+            logout_waiting(driver)
 
 
 
 
     except Exception as e:
-       print(e)
+        print(e)
 
 
 def logout_waiting(driver):
-
     while '1' == '1':
-        check_fun=driver.find_element_by_class_name('O90ur').text
+        check_fun = driver.find_element_by_class_name('O90ur').text
 
         if check_fun == 'online':
-
 
             None
 
 
         else:
 
-            print ('offline\n')
-            print(check_fun+'\n')
+            print('offline\n')
+            print(check_fun + '\n')
             print(datetime.now())
             online(driver)
 
 
-
-
 if __name__ == '__main__':
-    
-  message_sent=input('enter the sender mobile number' )
-  print("create a message api key in here https://smsapi.engineeringtgr.com/ and check a mail")
-  apikey=input("enter the apikey")
-  mobile_num=input("enter the login mobile number")
-  passw_pass=input("enter the login password")
-  target()
-    
+    message_sent = input('enter the sender mobile number')
+    print("create a message api key in here https://smsapi.engineeringtgr.com/ and check a mail")
+    apikey = input("enter the apikey")
+    mobile_num = input("enter the login mobile number")
+    passw_pass = input("enter the login password")
+    target()
+
